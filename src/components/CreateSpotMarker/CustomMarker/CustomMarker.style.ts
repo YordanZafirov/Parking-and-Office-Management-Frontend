e0c0b572e12f002445e9e7707eea6f294a5ea82d
GabrielaNikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2024-02-19 16:19:10 +0200

    feat(CustomMarker): add Custom Marker component

--- src/components/CreateSpotMarker/CustomMarker/CustomMarker.style.ts
+++ src/components/CreateSpotMarker/CustomMarker/CustomMarker.style.ts
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 
 const MarkerStyled = styled.button`
-    /* Set the marker size here */
     width: 1.2rem;
     height: 1.2rem;
     border-radius: 1.2rem;
-    /* Set the marker color here */
     background: var(--red);
 
     display: inline-block;
