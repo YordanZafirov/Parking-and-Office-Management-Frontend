cb5de65cc69a2a7ca01b68e6fbecd306dfd9512b
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-02-20 09:40:14 +0200

    feat(Location): add feature

--- src/static/routes.ts
+++ src/static/routes.ts
@@ -1,7 +1,7 @@
 export const route = {
-    home: "/",
-    location: "/location",
-    createLocation: "/createLocation",
-    calendar: "/calendar",
-    login: "/login",
+  home: "/",
+  location: "/location",
+  createLocation: "/createLocation",
+  calendar: "/calendar",
+  login: "/login",
 };
