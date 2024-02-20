966274b741b4742890cc4e4cbaca4ca81b042ae5
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-02-20 09:40:14 +0200

    feat(Location): add feature

--- src/hooks/Token/Token.hook.ts
+++ src/hooks/Token/Token.hook.ts
@@ -2,34 +2,33 @@ import { jwtDecode } from "jwt-decode";
 import { useQuery } from "react-query";
 
 interface Token {
-  id: string;
-  email: string;
-  role: string;
-  iat: number;
-  exp: number;
+    id: string;
+    sub?: string;
+    email: string;
+    role: string;
+    iat: number;
+    exp: number;
 }
 
 const useToken = () => {
-  const { data: validToken, status } = useQuery("accessToken", () =>
-    localStorage.getItem("accessToken")
-  );
+    const { data: validToken, status } = useQuery("access_token", () => localStorage.getItem("access_token"));
 
-  if (status === "loading") {
-    return null;
-  }
+    if (status === "loading") {
+        return null;
+    }
 
-  if (status === "error" || !validToken) {
-    return null;
-  }
+    if (status === "error" || !validToken) {
+        return null;
+    }
 
-  // Try to decode the token
-  try {
-    const decodedToken: Token = jwtDecode(validToken);
-    return decodedToken;
-  } catch (error) {
-    console.error("Error decoding token:", error);
-    return null; // Return null in case of decoding error
-  }
+    // Try to decode the token
+    try {
+        const decodedToken: Token = jwtDecode(validToken);
+        return decodedToken;
+    } catch (error) {
+        console.error("Error decoding token:", error);
+        return null; // Return null in case of decoding error
+    }
 };
 
 export default useToken;
---
+++ src/pages/Location/Location.static.ts
@@ -0,0 +1,8 @@
+export interface Location {
+    id?: string;
+    name: string;
+    city: string;
+    address: string;
+    imgUrl: string;
+    modifiedBy?: string;
+}
