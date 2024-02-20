7c32239d7e4bdd3d31c9931722257783568ff79f
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-02-20 09:40:14 +0200

    feat(Location): add feature

--- src/pages/Location/Location.tsx
+++ src/pages/Location/Location.tsx
@@ -1,30 +1,27 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
+// import Loader from "../../components/loader/Loader";
+// import useLocation from "./Location.logic";
 
-import Loader from "../../components/loader/Loader";
-import LocationTable from "./LocationTable/LocationTable";
-import SpotChoice from "./SpotChoice/SpotChoice";
-import useLocation from "./Location.logic";
+import LocationCreateForm from "./LocationCreate/LocationCreate";
 
 const Location = () => {
-  const { singleLocation, isLoading, error } = useLocation();
+    // const { singleLocation, isLoading, error } = useLocation();
+    // useEffect(() => {
+    //     console.log(singleLocation);
+    // }, [singleLocation]);
+    // if (isLoading) {
+    //     return <Loader />;
+    // }
+    // if (error) {
+    //     return <div>Error loading location</div>;
+    // }
+    // return <div></div>;
 
-  useEffect(() => {
-    console.log(singleLocation);
-  }, [singleLocation]);
-
-  if (isLoading) {
-    return <Loader />;
-  }
-
-  if (error) {
-    return <div>Error loading location</div>;
-  }
-  return (
-    <>
-      <SpotChoice />
-      <LocationTable />
-    </>
-  );
+    return (
+        <>
+            <LocationCreateForm />
+        </>
+    );
 };
 
 export default Location;
--- src/services/locationService.ts
+++ src/services/locationService.ts
@@ -1,15 +1,28 @@
-import { get } from "./fetchService";
+import { del, get, post } from "./fetchService";
 import { endpoints } from "../static/endpoints";
 import { Location } from "../pages/Home/Home.static";
+import { LocationCreate } from "../pages/Location/LocationCreate/LocationCreate.static";
 
 // Function to get all locations
 export const getLocations = async (): Promise<Location[]> => {
-  const response = await get(endpoints.getLocations, {});
-  return response;
+    const response = await get(endpoints.getLocations, {});
+    return response;
 };
 
 // Function to get location by id
 export const getLocation = async (id: string): Promise<Location[]> => {
-  const response = await get(endpoints.getLocations + "/" + id, {});
-  return response;
+    const response = await get(endpoints.getLocations + "/" + id, {});
+    return response;
+};
+
+// Function to add a new location
+export const addLocation = async (locationData: LocationCreate): Promise<LocationCreate> => {
+    const response = await post(endpoints.createLocation, locationData);
+    return response;
+};
+
+// Function to delete location by id
+export const deleteLocation = async (id: string): Promise<void> => {
+    const response = await del(endpoints.getLocations + "/" + id, {});
+    return response;
 };
