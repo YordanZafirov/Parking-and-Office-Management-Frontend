7b5334c421f1ae714b35715af23a24b91f0dab7f
GabrielaNikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2024-02-21 01:45:17 +0200

    fix(): resolving merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,49 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import Home from "./pages/Home/Home";
-import { GlobalStyle } from "./components/Global.style";
-import { route } from "./static/routes";
-import { AuthProvider } from "./context/AuthContext";
-import Navigation from "./components/navigation/Navigation";
-import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+import { GlobalStyle } from './components/Global.style';
+import { route } from './static/routes';
+import { AuthProvider } from './context/AuthContext';
+import Navigation from './components/navigation/Navigation';
+import styled from 'styled-components';
 
-import HomeCalendar from "./pages/Calendar-component/Home-calendar/Home";
-import UsersPage from "./pages/User/UsersPage/UsersPage";
-import Register from "./pages/Register/Register";
-import LocationCreateForm from "./pages/LocationCreate/LocationCreate";
-import Reservation from "./pages/Reservation/Reservation";
+import HomeCalendar from './pages/Calendar-component/Home-calendar/Home';
+import UsersPage from './pages/User/UsersPage/UsersPage';
+import Register from './pages/Register/Register';
+import LocationCreateForm from './pages/LocationCreate/LocationCreate';
+import Reservation from './pages/Reservation/Reservation';
 
 export const StyledLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
 `;
 
 export const StyledMain = styled.main`
-  flex: 1;
-  width: 100%;
+    flex: 1;
+    width: 100%;
 `;
 
 function App() {
-  return (
-    <div>
-      <GlobalStyle />
-      <BrowserRouter>
-        <AuthProvider>
-          <Navigation />
-          <Routes>
-            <Route path={route.home} element={<Home />} />
-            <Route path={`${route.location}/:id`} element={<Reservation />} />
-            <Route
-              path={route.createLocation}
-              element={<LocationCreateForm />}
-            />
-            <Route path={route.calendar} element={<HomeCalendar />} />
-            <Route path={route.login} element={<Login />} />
+    return (
+        <div>
+            <GlobalStyle />
+            <BrowserRouter>
+                <AuthProvider>
+                    <Navigation />
+                    <Routes>
+                        <Route path={route.home} element={<Home />} />
+                        <Route path={`${route.location}/:id`} element={<Reservation />} />
+                        <Route path={route.createLocation} element={<LocationCreateForm />} />
+                        <Route path={route.calendar} element={<HomeCalendar />} />
+                        <Route path={route.login} element={<Login />} />
                         <Route path={route.register} element={<Register />} />
                         <Route path={route.user} element={<UsersPage />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </div>
-  );
+                    </Routes>
+                </AuthProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
