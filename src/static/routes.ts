a4c19c0d4fd99f5e20190d407f4725aaace917c6
GabrielaNikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2024-02-21 02:19:20 +0200

    fix(package.json): remove double entries

--- src/static/routes.ts
+++ src/static/routes.ts
@@ -1,12 +1,12 @@
 export const route = {
-  home: '/',
-  login: '/login',
-  location: '/location',
-  createSpot: '/spot',
-  createFloorPlan: '/floor-plan',
-  createLocation: "/createLocation",
-  calendar: "/calendar",
-  user: "/user",
-  updateUser: "/user-update/:userId",
-  register: "/register"
+    home: '/',
+    login: '/login',
+    location: '/location',
+    createSpot: '/spot',
+    createFloorPlan: '/floor-plan',
+    createLocation: '/createLocation',
+    calendar: '/calendar',
+    user: '/user',
+    updateUser: '/user-update/:userId',
+    register: '/register',
 };
