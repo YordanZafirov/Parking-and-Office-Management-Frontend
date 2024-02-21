390edf5a44a636b76f8a8cf3eef2bc15f79dcfe2
Daniel Petrov <daniplpetrov@gmail.com>
2024-02-21 23:33:05 +0200

    fix(Global.styles.ts): change global colors

--- src/components/Global.style.ts
+++ src/components/Global.style.ts
@@ -2,11 +2,20 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 :root {
-  --light-pink: #e8bcb9;
-  --orange: #f49f5a;
-  --red: #ae445a;
-  --light-purple: #662549;
-  --purple: #451952;
-  --dark-blue: #1d1a39;
+  --grey: #3c4245;
+  --grey-light: #8a8e8f;
+  --grey-dark: #242829;
+  --blue-green: #729292;
+  --blue-green-light: #aabebe;
+  --blue-green-dark: #394949;
+  --pink: #dfcdc1;
+  --pink-light: #ece1da;
+  --pink-dark: #867b74;
+  --beige: #e7dfd6;
+  --beige-light: #f5f2ef;
+  --beige-dark: #8b8680;
+  --brown: #d0bba2;
+  --brown-light: #7d7061;
+  --brown-dark: #e3d6c7;
 }
 `;
