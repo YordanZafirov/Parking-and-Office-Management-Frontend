346ec0abbffe10a91bea8af6c78bcc8ccb2ca567
YordanZafirov <iordanzafirov17@gmail.com>
2024-02-21 11:20:44 +0200

    Refactor ReservationTable and SpotType components

--- src/pages/Reservation/ReservationTable/ReservationTable.tsx
+++ src/pages/Reservation/ReservationTable/ReservationTable.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import useReservationTableLogic from './ReservationTable.logic';
 import { ReservationTableInterface } from './ReservationTable.static';
+import { ReservationTableStyle } from './ReservationTable.style';
+import { Container } from '../../../components/CommonStyledElements';
 
 const ReservationTable = () => {
     const { formattedReservations } = useReservationTableLogic();
@@ -10,31 +12,37 @@ const ReservationTable = () => {
     }, [formattedReservations]);
 
     return (
-        <table>
-            <caption>My reservations</caption>
-            <thead>
-                <tr>
-                    <th>Spot</th>
-                    <th>Start</th>
-                    <th>End</th>
-                    <th>Comment</th>
-                    <th>Spot Description</th>
-                </tr>
-            </thead>
-            <tbody>
-                {formattedReservations.map((reservation: ReservationTableInterface, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>{reservation.spotName}</td>
-                            <td>{new Date(reservation.start).toLocaleString()}</td>
-                            <td>{new Date(reservation.end).toLocaleString()}</td>
-                            <td>{reservation.comment}</td>
-                            <td>{reservation.spotDescription}</td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+        <Container>
+            <ReservationTableStyle>
+                <caption>Future Reservations</caption>
+                <thead>
+                    <tr>
+                        <th>Spot</th>
+                        <th>Start</th>
+                        <th>End</th>
+                        <th>Comment</th>
+                        <th>Spot Description</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {formattedReservations.map((reservation: ReservationTableInterface, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{reservation.spotName}</td>
+                                <td>{new Date(reservation.start).toLocaleString()}</td>
+                                <td>{new Date(reservation.end).toLocaleString()}</td>
+                                <td>{reservation.comment}</td>
+                                <td>{reservation.spotDescription}</td>
+                                <td>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </ReservationTableStyle>
+        </Container>
     );
 };
 
