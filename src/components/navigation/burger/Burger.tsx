684ca958d5e3e93bfa094e3677524089de6d4496
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-02-22 17:59:27 +0200

    fix(Admin): guard admin

--- src/components/navigation/burger/Burger.tsx
+++ src/components/navigation/burger/Burger.tsx
@@ -1,19 +1,26 @@
-import RightNav from "../right-nav/RightNav";
-import useBurger from "./Burger.logic";
-import { BurgerDiv } from "./Burger.style";
+import { NavLink } from 'react-router-dom';
+import UserRoleHOC from '../../../pages/UserRoleHOC';
+import RightNav from '../right-nav/RightNav';
+import useBurger from './Burger.logic';
+import { BurgerDiv } from './Burger.style';
 
 const Burger = () => {
-  const { open, handleClick } = useBurger();
-  return (
-    <>
-      <BurgerDiv open={open} onClick={handleClick}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </BurgerDiv>
-      <RightNav open={open} handleClick={handleClick} />
-    </>
-  );
+    const { open, handleClick } = useBurger();
+    return (
+        <>
+            <BurgerDiv open={open} onClick={handleClick}>
+                <UserRoleHOC>
+                    <NavLink to={`/admin`}>
+                        <li>Admin</li>
+                    </NavLink>
+                </UserRoleHOC>
+                <div></div>
+                <div></div>
+                <div></div>
+            </BurgerDiv>
+            <RightNav open={open} handleClick={handleClick} />
+        </>
+    );
 };
 
 export default Burger;
