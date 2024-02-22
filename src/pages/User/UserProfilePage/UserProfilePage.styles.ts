9e4e45bb2ad7070bb14fb27a86cbf156eea796b8
Daniel Petrov <daniplpetrov@gmail.com>
2024-02-22 14:35:08 +0200

    fix(user): make user profile page responsive

--- src/pages/User/UserProfilePage/UserProfilePage.styles.ts
+++ src/pages/User/UserProfilePage/UserProfilePage.styles.ts
@@ -9,6 +9,14 @@ export const ToggleButtonsContainer = styled.div`
     overflow: hidden;
     align-items: center;
     justify-content: center;
+    background-color: var(--beige-light);
+    border-radius: 8px;
+
+    @media (max-width: 520px) {
+        margin: 20px auto 30px;
+        flex-direction:column;
+        gap: 5px  /* When screen width is at least 700px, use two columns */
+    }
 `;
 
 export const UpdateButtonContainer = styled.div`
@@ -43,7 +51,7 @@ export const StyledDetailBullet = styled.li`
 `;
 
 export const UserProfileContainer = styled.div`
-    margin: 30px auto;
+    margin: 30px 10px;
     display: grid;
     grid-template-columns: 60% 40%;
     gap: 20px;
@@ -52,6 +60,19 @@ export const UserProfileContainer = styled.div`
     background-color: var(--beige-light);
     border-radius: 8px;
     overflow: hidden;
+
+    @media (min-width: 1500px) {
+        margin: 30px auto;
+    }
+    @media (max-width: 700px) {
+        grid-template-columns: 1fr;
+        > :nth-child(2) {
+            display: none;
+        }
+    }
+    @media (max-width: 510px) {
+        margin: 10px;
+    }
 `;
 
 export const UserAdditionalInfoContainer = styled.div`
@@ -63,6 +84,7 @@ export const UserAdditionalInfoContainer = styled.div`
     background-color: var(--brown);
     border-radius: 8px;
     text-align: center;
+    justify-content: center;
 `;
 export const UserMainInfoContainer = styled.div`
     display: flex;
@@ -76,6 +98,10 @@ export const UserMainInfoContainer = styled.div`
     align-items: center;
     justify-content: center;
     text-align: center;
+
+    @media (max-width: 510px) {
+        flex-direction:column;
+    }
 `;
 export const UserProfilePictureContainer = styled.div`
     display: flex;
