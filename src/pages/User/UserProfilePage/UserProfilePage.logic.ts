10f12f9b59e26c08572c30216ac1a14b733da758
Daniel Petrov <daniplpetrov@gmail.com>
2024-02-22 22:50:33 +0200

    feat(user): add UserReservationsTable
    Add the Table component to Components folder. Use the Table component in user profile page to display the user's different reservations

--- src/pages/User/UserProfilePage/UserProfilePage.logic.ts
+++ src/pages/User/UserProfilePage/UserProfilePage.logic.ts
@@ -1,71 +1,91 @@
-import { useLocation, useNavigate, useParams } from "react-router";
-import { useQuery } from "react-query";
+import { useLocation, useNavigate, useParams } from 'react-router';
+import { useQuery } from 'react-query';
 import {
-  getCurrentReservationsByUser,
-  getFutureReservationsByUser,
-  getPastReservationsByUser,
-  getUser,
-} from "../../../services/userService";
-import { route } from "../../../static/routes";
-import { SetStateAction, useState } from "react";
-import { useAuth } from "../../../context/AuthContext";
+    getCurrentReservationsByUser,
+    getFutureReservationsByUser,
+    getPastReservationsByUser,
+    getUser,
+} from '../../../services/userService';
+import { route } from '../../../static/routes';
+import { SetStateAction, useState } from 'react';
+import { useAuth } from '../../../context/AuthContext';
 
-export const UseUser = (userId: string | undefined) => {
-  const { data: user, refetch: userRefetch } = useQuery(["user", userId], () =>
-    getUser(userId)
-  );
-  return { user, userRefetch };
+export const useUser = (userId: string | undefined) => {
+    const { data: user, refetch: userRefetch } = useQuery(['user', userId], () => getUser(userId));
+    return { user, userRefetch };
 };
 
-export const UsePastReservationsByUser = (userId: string | undefined) => {
-
-  const { data: reservations, refetch } = useQuery(["pastReservationsByUserId"], () =>
-    getPastReservationsByUser(userId)
-  );
-  return { pastReservations: reservations, refetch };
+export const usePastReservationsByUser = (userId: string | undefined) => {
+    const {
+        data: reservations,
+        refetch,
+        isLoading,
+    } = useQuery(['pastReservationsByUserId'], () => getPastReservationsByUser(userId));
+    return { pastReservations: reservations, pastReservationsRefetch: refetch, arePastReservationsLoading: isLoading };
 };
-export const UseCurrentReservationsByUser = (userId: string | undefined) => {
-  const { data: reservations, refetch } = useQuery(["currentReservationsByUserId"], () =>
-    getCurrentReservationsByUser(userId)
-  );
-  return { currentReservations: reservations, refetch };
+export const useCurrentReservationsByUser = (userId: string | undefined) => {
+    const {
+        data: reservations,
+        refetch,
+        isLoading,
+    } = useQuery(['currentReservationsByUserId'], () => getCurrentReservationsByUser(userId));
+    return {
+        currentReservations: reservations,
+        currentReservationsRefetch: refetch,
+        areCurrentReservationsLoading: isLoading,
+    };
 };
-export const UseFutureReservationsByUser = (userId: string | undefined) => {
-  const { data: reservations, refetch } = useQuery(["futureReservationsByUserId"], () =>
-    getFutureReservationsByUser(userId)
-  );
-  return { futureReservations: reservations, refetch };
+export const useFutureReservationsByUser = (userId: string | undefined) => {
+    const {
+        data: reservations,
+        refetch,
+        isLoading,
+    } = useQuery(['futureReservationsByUserId'], () => getFutureReservationsByUser(userId));
+    return {
+        futureReservations: reservations,
+        futureReservationsRefetch: refetch,
+        areFutureReservationsLoading: isLoading,
+    };
 };
 
 export const UserProfilePageLogic = () => {
-const {id: userId} = useParams()
-  const { user, userRefetch } = UseUser(userId);
-  const { pastReservations } = UsePastReservationsByUser(userId);
-  const { currentReservations } = UseCurrentReservationsByUser(userId);
-  const { futureReservations } = UseFutureReservationsByUser(userId);
-  console.log([futureReservations]);
-  const [activeTab, setActiveTab] = useState("future"); // State to track active tab
-  const {logout} = useAuth();
-  const handleTabClick = (tab: SetStateAction<string>) => {
-    setActiveTab(tab);
-  };
+    const { id: userId } = useParams();
+    const { user, userRefetch } = useUser(userId);
+    const { pastReservations, arePastReservationsLoading, pastReservationsRefetch } = usePastReservationsByUser(userId);
+    const { currentReservations, areCurrentReservationsLoading, currentReservationsRefetch } =
+        useCurrentReservationsByUser(userId);
+    const { futureReservations, areFutureReservationsLoading, futureReservationsRefetch } =
+        useFutureReservationsByUser(userId);
+    console.log([futureReservations]);
+    const [activeTab, setActiveTab] = useState('future'); // State to track active tab
+    const { logout } = useAuth();
+    const handleTabClick = (tab: SetStateAction<string>) => {
+        setActiveTab(tab);
+    };
 
-  const navigate = useNavigate();
-  const location = useLocation();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const reservationTypes = {past: 'Past', current: 'Current', future: 'Future'};
 
-
-  return {
-    user,
-    pastReservations,
-    currentReservations,
-    futureReservations,
-    activeTab,
-    handleTabClick,
-    userRefetch,
-    logout,
-    handleUpdateUserProfilePicture: (id: string) =>
-    navigate(`${route.user}/${id}/change-picture`, { state: { background: location } }),
-    handleUpdateUserPassword: (id: string) =>
-    navigate(`${route.user}/${id}/change-password`, { state: { background: location } }),
-  };
+    return {
+        user,
+        pastReservations,
+        arePastReservationsLoading,
+        pastReservationsRefetch,
+        currentReservations,
+        areCurrentReservationsLoading,
+        currentReservationsRefetch,
+        futureReservations,
+        areFutureReservationsLoading,
+        futureReservationsRefetch,
+        activeTab,
+        handleTabClick,
+        userRefetch,
+        logout,
+        reservationTypes,
+        handleUpdateUserProfilePicture: (id: string) =>
+            navigate(`${route.user}/${id}/change-picture`, { state: { background: location } }),
+        handleUpdateUserPassword: (id: string) =>
+            navigate(`${route.user}/${id}/change-password`, { state: { background: location } }),
+    };
 };
