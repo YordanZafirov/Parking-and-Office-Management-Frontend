fc1f5164a9f3d26d1b8e6daea94227d49d4ac82a
YordanZafirov <iordanzafirov17@gmail.com>
2024-02-22 14:03:58 +0200

    feat(Reservation): Update global and navigation styles, add ReservationContainer, and modify SpotType styles

--- src/pages/Reservation/ReservationTable/ReservationTable.style.ts
+++ src/pages/Reservation/ReservationTable/ReservationTable.style.ts
@@ -1,27 +1,74 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ReservationTableStyle = styled.table`
-  border-collapse: collapse;
-  width: 100%;
-  margin-top: 1rem;
+    border-collapse: collapse;
+    width: 100%;
+    margin: 0 auto;
+    margin-top: 1rem;
+    text-align: center;
+    font-size: 1.2rem;
 
-  caption {
-    font-size: 1.5rem;
-    margin-bottom: 1rem;
-  }
+    caption {
+        font-size: 1.5rem;
+        margin-bottom: 1rem;
+    }
 
-  th,
-  td {
-    border: 1px solid #ddd;
-    padding: 8px;
-    text-align: left;
-  }
+    .table-head {
+        background-color: var(--beige-light);
+    }
 
-  th {
-    background-color: #f2f2f2;
-  }
+    th,
+    td {
+        background-color: var(--blue-green-light);
+        border: 1px solid var(--blue-green-dark);
+        padding: 8px;
+    }
 
-  tr:nth-child(even) {
-    background-color: #f2f2f2;
-  }
+    th {
+        background-color: #f2f2f2;
+    }
+
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    button {
+        background-color: var(--beige);
+        color: var(--grey-dark);
+        border: none;
+        padding: 5px 10px;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: var(--beige-light);
+    }
+
+    @media (max-width: 768px) {
+        font-size: 1rem;
+        border-collapse: unset;
+
+        th {
+            display: none;
+        }
+
+        td {
+            display: block;
+            text-align: left;
+            border: none;
+
+            button {
+                margin: 0 auto;
+                display: block;
+            }
+        }
+
+        td::before {
+            content: attr(data-label);
+            float: left;
+            font-weight: bold;
+            margin-right: 10px;
+        }
+    }
 `;
--- src/pages/Reservation/ReservationTable/ReservationTable.tsx
+++ src/pages/Reservation/ReservationTable/ReservationTable.tsx
@@ -13,39 +13,41 @@ const ReservationTable = () => {
 
     return (
         <Container>
-            {formattedReservations.length === 0 ? (
-                <PageTitle>No future reservations</PageTitle>
-            ) : (
-                <ReservationTableStyle>
-                    <caption>Future Reservations</caption>
-                    <thead>
-                        <tr>
-                            <th>Spot</th>
-                            <th>Spot Description</th>
-                            <th>Comment</th>
-                            <th>Start</th>
-                            <th>End</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {formattedReservations.map((reservation: ReservationTableInterface) => {
-                            return (
-                                <tr key={reservation.id}>
-                                    <td>{reservation.spotName}</td>
-                                    <td>{reservation.spotDescription}</td>
-                                    <td>{reservation.comment}</td>
-                                    <td>{new Date(reservation.start).toLocaleString()}</td>
-                                    <td>{new Date(reservation.end).toLocaleString()}</td>
-                                    <td>
-                                        <button onClick={() => deleteReservation(reservation.id)}>Delete</button>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </ReservationTableStyle>
-            )}
+            <div>
+                {formattedReservations.length === 0 ? (
+                    <PageTitle>No future reservations</PageTitle>
+                ) : (
+                    <ReservationTableStyle>
+                        <caption>Future Reservations</caption>
+                        <thead>
+                            <tr>
+                                <th className='table-head'>Spot</th>
+                                <th className='table-head'>Spot Description</th>
+                                <th className='table-head'>Comment</th>
+                                <th className='table-head'>Start</th>
+                                <th className='table-head'>End</th>
+                                <th className='table-head'>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {formattedReservations.map((reservation: ReservationTableInterface) => {
+                                return (
+                                    <tr key={reservation.id}>
+                                        <td data-label="Spot:">{reservation.spotName}</td>
+                                        <td data-label="Spot Description:">{reservation.spotDescription}</td>
+                                        <td data-label="Comment:">{reservation.comment}</td>
+                                        <td data-label="Start:">{new Date(reservation.start).toLocaleString()}</td>
+                                        <td data-label="End:">{new Date(reservation.end).toLocaleString()}</td>
+                                        <td>
+                                            <button onClick={() => deleteReservation(reservation.id)}>Delete</button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </ReservationTableStyle>
+                )}
+            </div>
         </Container>
     );
 };
