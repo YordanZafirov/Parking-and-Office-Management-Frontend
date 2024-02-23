36f2a5dbe549c2a0c92aa745a0929e156521fe24
YordanZafirov <iordanzafirov17@gmail.com>
2024-02-23 11:24:08 +0200

    Refactor table styles and remove unused code

--- src/components/navigation/burger/Burger.tsx
+++ src/components/navigation/burger/Burger.tsx
@@ -1,5 +1,3 @@
-import { NavLink } from 'react-router-dom';
-import UserRoleHOC from '../../../pages/UserRoleHOC';
 import RightNav from '../right-nav/RightNav';
 import useBurger from './Burger.logic';
 import { BurgerDiv } from './Burger.style';
@@ -9,11 +7,7 @@ const Burger = () => {
     return (
         <>
             <BurgerDiv open={open} onClick={handleClick}>
-                <UserRoleHOC>
-                    <NavLink to={`/admin`}>
-                        <li>Admin</li>
-                    </NavLink>
-                </UserRoleHOC>
+
                 <div></div>
                 <div></div>
                 <div></div>
