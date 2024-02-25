91855f6bb8b63be182e905c44d21f2833e031587
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-02-25 23:30:20 +0200

    feat(FloorPlan): edit and delete

--- src/pages/FloorPlan/FloorPlan.tsx
+++ src/pages/FloorPlan/FloorPlan.tsx
@@ -5,11 +5,24 @@ import { ListContainer, LocationTableStyle } from './FloorPlan.style';
 import DeleteIcon from '../../components/icons/DeleteIcon';
 import useModal from '../../components/ModalList/useModal';
 import DeleteFloorPlanModal from './FloorPlanDetails/FloorPlanListModal/DeleteModal';
+import EditIcon from '../../components/icons/EditIcon';
+import EditFloorPlanModal from './FloorPlanDetails/FloorPlanListModal/EditModal';
 
 const FloorPlanPage = () => {
-    const { floorPlan, onDeleteClick, onDeleteConfirm } = useFloorPlan();
+    const {
+        floorPlan,
+        onDeleteClick,
+        onDeleteConfirm,
+        onEditClick,
+        onEditConfirm,
+        setCurrentFloorPlanName,
+        currentFloorPlanImage,
+        setCurrentFloorPlanImage,
+        originalFloorPlanName,
+    } = useFloorPlan();
 
     const { isVisible: isDeleteModalVisible, showModal: showDeleteModal, hideModal: hideDeleteModal } = useModal();
+    const { isVisible: isEditModalVisible, showModal: showEditModal, hideModal: hideEditModal } = useModal();
 
     return (
         <ListContainer>
@@ -32,6 +45,17 @@ const FloorPlanPage = () => {
                                 </td>
 
                                 <td>
+                                    <EditIcon
+                                        onClick={() => {
+                                            onEditClick(
+                                                floorPlanItem.id || '',
+                                                floorPlanItem.name || '',
+                                                floorPlanItem.imgUrl || '',
+                                            );
+                                            showEditModal();
+                                        }}
+                                    />
+
                                     <DeleteIcon
                                         onClick={() => {
                                             onDeleteClick(floorPlanItem.id || '');
@@ -54,6 +78,18 @@ const FloorPlanPage = () => {
                     isVisible={isDeleteModalVisible}
                     hideModal={hideDeleteModal}
                     onDeleteConfirm={onDeleteConfirm}
+                />
+            )}
+
+            {isEditModalVisible && (
+                <EditFloorPlanModal
+                    isVisible={isEditModalVisible}
+                    hideModal={hideEditModal}
+                    onConfirm={onEditConfirm}
+                    currentFloorPlanName={originalFloorPlanName}
+                    currentFloorPlanImage={currentFloorPlanImage}
+                    setCurrentFloorPlanName={setCurrentFloorPlanName}
+                    setCurrentFloorPlanImage={setCurrentFloorPlanImage}
                 />
             )}
         </ListContainer>
