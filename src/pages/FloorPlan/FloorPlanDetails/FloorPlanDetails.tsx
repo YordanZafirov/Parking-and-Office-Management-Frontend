6a6c173525d302c57bc5e582b184367d65931cad
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-02-25 13:09:27 +0200

    fix(Admin): add new column Floor Plan

--- src/pages/FloorPlan/FloorPlanDetails/FloorPlanDetails.tsx
+++ src/pages/FloorPlan/FloorPlanDetails/FloorPlanDetails.tsx
@@ -1,33 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { FloorPlanDetailsContainer, HeadingFloorPlan } from './FloorPlanDetails.style';
-import { getFloorPlan } from '../../../services/floorPlanService';
-import { FloorPlan } from '../FloorPlan.static';
+// import EditIcon from '../../../components/icons/EditIcon';
+import DeleteIcon from '../../../components/icons/DeleteIcon';
+import useModal from '../../../components/ModalList/useModal';
+import DeleteFloorPlanModal from './FloorPlanListModal/DeleteModal';
+import useFloorPlanDetails from './FloorPlanDetails.logic';
 
-const FloorPlanDetails = () => {
-    const { id } = useParams();
-    console.log('useParams ID:', id);
-    const [floorPlan, setFloorPlan] = useState<FloorPlan | null>(null);
+interface FloorPlansDetailsProps {
+    floorPlans: FloorPlanProp[];
+}
 
-    useEffect(() => {
-        console.log('Effect is running');
-        console.log('ID:', id);
-        if (id) {
-            // Fetch floor plan details based on the ID
-            getFloorPlan(id)
-                .then((data: FloorPlan) => {
-                    console.log('Fetched data:', data);
-                    setFloorPlan(data);
-                })
-                .catch((error) => console.error('Error fetching floor plan details:', error));
-        }
-    }, [id]);
+const FloorPlanDetails: React.FC<FloorPlansDetailsProps> = ({ floorPlans }) => {
+    const { onDeleteClick, onDeleteConfirm } = useFloorPlanDetails();
 
-    console.log('Floor Plan State:', floorPlan);
-
-    if (!floorPlan) {
-        return <div>Loading...</div>; // You may want to add a loading state
-    }
+    const { isVisible: isDeleteModalVisible, showModal: showDeleteModal, hideModal: hideDeleteModal } = useModal();
 
     return (
         <FloorPlanDetailsContainer>
