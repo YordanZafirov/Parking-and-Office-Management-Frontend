f35e69e2f98e25d9b34e533b142da1fb63d85a25
YordanZafirov <iordanzafirov17@gmail.com>
2024-02-26 12:49:49 +0200

    Refactor FloorPlanDetails component to fetch and display floor plan details

---
+++ src/pages/FloorPlan/FloorPlan.logic.ts
@@ -0,0 +1,127 @@
+import { useEffect, useState } from 'react';
+import { deleteFloorPlan, getFloorPlans, updateFloorPlan } from '../../services/floorPlanService';
+import { FloorPlan as FloorPlanProp } from './FloorPlan.static';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import useToken from '../../hooks/Token/Token.hook';
+
+function useFloorPlan() {
+    const { data: floorPlans, isLoading, error, refetch } = useQuery('floorPlan', () => getFloorPlans());
+    const decodedToken = useToken();
+
+    const [floorPlan, setFloorPlan] = useState<FloorPlanProp[]>([]);
+    const location = useLocation();
+    const { state } = location;
+    const { locationId } = state || {};
+    const navigate = useNavigate();
+
+    const [selectedFloorPlanIdForDelete, setSelectedFloorPlanIdForDelete] = useState<string | null>(null);
+    const [selectedFloorPlanIdForEdit, setSelectedFloorPlanIdForEdit] = useState<string | null>(null);
+    const [currentFloorPlanName, setCurrentFloorPlanName] = useState<string>('');
+    const [currentFloorPlanImage, setCurrentFloorPlanImage] = useState<string>('');
+    const [originalFloorPlanName, setOriginalFloorPlanName] = useState<string>('');
+
+    useEffect(() => {
+        const fetchFloorPlansData = async () => {
+            try {
+                const fetchedFloorPlans = await getFloorPlans();
+
+                if (fetchedFloorPlans && fetchedFloorPlans.length > 0) {
+                    const matchingFloorPlan: FloorPlanProp[] = fetchedFloorPlans.filter(
+                        (plan) => plan.locationId === locationId,
+                    );
+
+                    if (matchingFloorPlan.length > 0) {
+                        setFloorPlan(matchingFloorPlan);
+                    } else {
+                        setFloorPlan([]);
+                    }
+                } else {
+                    setFloorPlan([]);
+                }
+            } catch (error) {
+                console.error('Error fetching floor plans data:', error);
+            }
+        };
+
+        fetchFloorPlansData();
+    }, [locationId, navigate]);
+
+    const onDeleteFloorPlan = async (floorPlanId: string) => {
+        try {
+            console.log('Deleting floor plan...');
+            await deleteFloorPlan(floorPlanId);
+            console.log('Floor plan deleted successfully');
+        } catch (error) {
+            console.error('Error deleting floor plan:', error);
+        }
+    };
+
+    const onDeleteClick = (floorPlanId: string) => {
+        setSelectedFloorPlanIdForDelete(floorPlanId);
+    };
+
+    const onDeleteConfirm = async () => {
+        if (selectedFloorPlanIdForDelete) {
+            await onDeleteFloorPlan(selectedFloorPlanIdForDelete);
+            setSelectedFloorPlanIdForDelete(null);
+        }
+    };
+
+    const onEditFloorPlan = async (floorPlanId: string, newFloorPlanData: FloorPlanProp) => {
+        try {
+            if (newFloorPlanData.name !== originalFloorPlanName) {
+                await updateFloorPlan(floorPlanId, newFloorPlanData);
+            } else {
+                const { name: _, ...updatedFloorPlanData } = newFloorPlanData;
+                await updateFloorPlan(floorPlanId, updatedFloorPlanData);
+            }
+
+            refetch();
+        } catch (error) {
+            console.error('Error editing location:', error);
+        }
+    };
+
+    const onEditClick = (floorPlanId: string, floorPlanName: string, floorPlanImage: string) => {
+        setSelectedFloorPlanIdForEdit(floorPlanId);
+        setCurrentFloorPlanName(floorPlanName);
+        setCurrentFloorPlanImage(floorPlanImage);
+        setOriginalFloorPlanName(floorPlanName);
+    };
+
+    const onEditConfirm = async () => {
+        try {
+            if (selectedFloorPlanIdForEdit) {
+                const newFloorPlanData = {
+                    name: currentFloorPlanName,
+                    image: currentFloorPlanImage,
+                    modifiedBy: decodedToken?.id,
+                };
+
+                await onEditFloorPlan(selectedFloorPlanIdForEdit, newFloorPlanData);
+            }
+
+            setSelectedFloorPlanIdForEdit(null);
+            setCurrentFloorPlanName('');
+            setCurrentFloorPlanImage('');
+        } catch (error) {
+            console.error('Error handling edit confirmation:', error);
+        }
+    };
+    return {
+        floorPlan,
+        onDeleteClick,
+        onDeleteConfirm,
+        onEditClick,
+        onEditConfirm,
+        currentFloorPlanName,
+        setCurrentFloorPlanName,
+        currentFloorPlanImage,
+        setCurrentFloorPlanImage,
+        originalFloorPlanName,
+        setOriginalFloorPlanName,
+    };
+}
+
+export default useFloorPlan;
--- src/pages/FloorPlan/FloorPlanDetails/FloorPlanDetails.tsx
+++ src/pages/FloorPlan/FloorPlanDetails/FloorPlanDetails.tsx
@@ -1,20 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { FloorPlanDetailsContainer, HeadingFloorPlan } from './FloorPlanDetails.style';
-// import EditIcon from '../../../components/icons/EditIcon';
-import DeleteIcon from '../../../components/icons/DeleteIcon';
-import useModal from '../../../components/ModalList/useModal';
-import DeleteFloorPlanModal from './FloorPlanListModal/DeleteModal';
-import useFloorPlanDetails from './FloorPlanDetails.logic';
+import { getFloorPlan } from '../../../services/floorPlanService';
+import { FloorPlan } from '../FloorPlan.static';
 
-interface FloorPlansDetailsProps {
-    floorPlans: FloorPlanProp[];
-}
+const FloorPlanDetails = () => {
+    const { id } = useParams();
+    console.log('useParams ID:', id);
+    const [floorPlan, setFloorPlan] = useState<FloorPlan | null>(null);
 
-const FloorPlanDetails: React.FC<FloorPlansDetailsProps> = ({ floorPlans }) => {
-    const { onDeleteClick, onDeleteConfirm } = useFloorPlanDetails();
+    useEffect(() => {
+        console.log('Effect is running');
+        console.log('ID:', id);
+        if (id) {
+            // Fetch floor plan details based on the ID
+            getFloorPlan(id)
+                .then((data: FloorPlan) => {
+                    console.log('Fetched data:', data);
+                    setFloorPlan(data);
+                })
+                .catch((error) => console.error('Error fetching floor plan details:', error));
+        }
+    }, [id]);
 
-    const { isVisible: isDeleteModalVisible, showModal: showDeleteModal, hideModal: hideDeleteModal } = useModal();
+    console.log('Floor Plan State:', floorPlan);
+
+    if (!floorPlan) {
+        return <div>Loading...</div>; // You may want to add a loading state
+    }
 
     return (
         <FloorPlanDetailsContainer>
