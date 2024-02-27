b553011ca1a3d8bf214eddb962a551b0cf931f11
GabrielaNikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2024-02-27 10:54:05 +0200

    reafctor(CustomSpotMarker): add spot info on hover, change style

--- src/pages/CreateSpots/CustomSpotMarker/CustomSpotMarker.tsx
+++ src/pages/CreateSpots/CustomSpotMarker/CustomSpotMarker.tsx
@@ -1,5 +1,23 @@
-import { MarkerStyled } from './CustomSpotMarker.style';
+import { SpotMarker } from '../AddSpotForm/AddSpotForm.static';
+import { MarkerStyled, StyledToolTip } from './CustomSpotMarker.style';
 
-export default function CustomMarker() {
-    return <MarkerStyled />;
+export default function CustomMarker(props: SpotMarker) {
+    return (
+        <>
+            <MarkerStyled
+                data-tooltip-id={`component_${props.id}`}
+                data-tooltip-place="top"
+                className={`custom-marker_${props.isPermanent}`}
+            />
+            <StyledToolTip id={`component_${props.id}`} className="spot-info">
+                {
+                    <>
+                        <h4>{props.name}</h4>
+                        <p>{props.description}</p>
+                        <p>{props.isPermanent ? 'Spot is permanently occupied!' : null}</p>
+                    </>
+                }
+            </StyledToolTip>
+        </>
+    );
 }
